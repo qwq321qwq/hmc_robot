5ac3eada02978001905fdd7a8d6a981f21be3235
Tadele Belay Tuli <48058146+tulifams@users.noreply.github.com>
2019-03-26 10:56:39 +0100

    Update motion_planning.py

--- motion_planning.py
+++ motion_planning.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 ''' Author: Tadele Belay Tuli,  MSc., MSc.
     (c) 2018/9 University of Siegen, FAMS '''
+
+# Copyright Tadele Belay Tuli, 2019
+# Distributed under the terms of the GNU General Public License
+
+# --------------------------------------------------------------------
+# This function is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This function is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyspread.  If not, see <http://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------
    
 import time
 import datetime
